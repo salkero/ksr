b4ea7ff4fdb4427d3484fd82b603d93ea658f6a8
Chhour <e1595729@cmaisonneuve.qc.ca>
2019-12-03 12:02:46 -0500

    ajout de mot pour les pages visitees

---
+++ admin/language/en-gb/visited/visited.php
@@ -0,0 +1,5 @@
+<?php
+
+$_['']         = '';
+$_['heading_title']         = 'Visited Pages';
+$_['text_home']             = 'Visited Pages';
---
+++ admin/language/fr-fr/visited/visited.php
@@ -0,0 +1,5 @@
+<?php
+
+$_['']         = '';
+$_['heading_title']         = 'Pages visitées';
+$_['text_home']             = 'Pages visitées';
