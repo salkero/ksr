50efbb9eaccbb4ca27844d4359d7de636ad76161
Salhi <e1895293@cmaisonneuve.qc.ca>
2019-12-03 09:06:19 -0500

    modification de la table skr_visited ajout de deux colonnes 'url_modifie et nombre_vues' avec la création d'kun nouveau fichier SQL

---
+++ db/tableVisited.sql
@@ -0,0 +1,11 @@
+CREATE TABLE ksr_visited (
+    visited_id int NOT NULL AUTO_INCREMENT,
+    url varchar(150),
+    title varchar(35),
+    date DATETIME,
+    ip_address varchar(15),
+    user_id int,
+    url_modifie varchar(150),
+    nombre_vues int,
+    PRIMARY KEY (visited_id)
+);
