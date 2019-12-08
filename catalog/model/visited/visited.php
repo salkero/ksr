76e5d93afe9ce884fa94bc681a3ba5ba7cb81e0e
salkero <salkero@gmail.com>
2019-12-08 14:15:28 -0500

    modification requte add

--- catalog/model/visited/visited.php
+++ catalog/model/visited/visited.php
@@ -1,17 +1,17 @@
 <?php
 class ModelVisitedVisited extends Model {
     //fonction d'ajout d'une visite.
-    public function addVisit($url, $title, $date, $ip_address, $user_id, $url_modifie) {
+    public function addVisit($url, $title, $date, $ip_address, $url_modifie, $user_id) {
 
 
-                         $this->db->query("INSERT INTO ". DB_PREFIX . "visited (url, title, date, ip_address, user_id, url_modifie) 
+                         $this->db->query("INSERT INTO ". DB_PREFIX . "visited (url, title, date, ip_address, url_modifie, user_id) 
                          VALUES (
                           '".$this->db->escape($url)."',
                           '".$this->db->escape($title)."',
                           '".$this->db->escape($date)."',
                           '".$this->db->escape($ip_address)."',
-                          ".$this->db->escape($user_id).",
-                          '".$this->db->escape($url_modifie)."')");
+                          '".$this->db->escape($url_modifie)."',
+                          ".$this->db->escape($user_id).")");
     }
 
     //fonction de suppression.                    
@@ -34,12 +34,12 @@ class ModelVisitedVisited extends Model {
         return $resultat;
      }
      
-     // //fonction de section des dernieres 15 visites. 
-     public function getLastFifteenVisits(){
-        //SELECT * FROM ksr_visited ORDER BY `ksr_visited`.`visited_id` DESC LIMIT 15 
-        $resultat = $this->db->query("SELECT * FROM " .DB_PREFIX . "visited ORDER BY " .DB_PREFIX ."visited"."visited_id" DESC LIMIT 15);
-        return $resultat;
-     }
+    //  // //fonction de section des dernieres 15 visites. 
+    //  public function getLastFifteenVisits(){
+    //     //SELECT * FROM ksr_visited ORDER BY `ksr_visited`.`visited_id` DESC LIMIT 15 
+    //     $resultat = $this->db->query("SELECT * FROM " .DB_PREFIX . "visited ORDER BY " .DB_PREFIX ."visited"."visited_id" DESC LIMIT 15);
+    //     return $resultat;
+    //  }
 
      
     
