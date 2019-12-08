604b94f86564dd14c56df8cb412ee2bfc9cc1215
salkero <salkero@gmail.com>
2019-12-08 16:59:36 -0500

    correction du fichier mauvais transfert

---
+++ admin/language/en-gb/visited/visited_pages.php
@@ -0,0 +1,15 @@
+<?php
+
+$_['']         = '';
+$_['heading_title']         = 'Visited Pages';
+$_['text_home']             = 'Visited Pages';
+
+
+// Column
+$_['column_url']            = 'Url';
+$_['column_title']          = 'Title';
+$_['column_date']           = 'Date';
+$_['column_ip_adress']      = 'IP adress';
+$_['column_url_modifie']    = 'Modified url';
+$_['column_user_id']        = 'User id';
+
