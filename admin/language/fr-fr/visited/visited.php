046c80a573ed872f900ca22714b0850dc56e48de
salkero <salkero@gmail.com>
2019-12-08 16:15:33 -0500

    ajout des langages pour visited

--- admin/language/fr-fr/visited/visited.php
+++ admin/language/fr-fr/visited/visited.php
@@ -3,3 +3,11 @@
 $_['']         = '';
 $_['heading_title']         = 'Pages visitées';
 $_['text_home']             = 'Pages visitées';
+
+// Column
+$_['column_url']            = 'Url';
+$_['column_title']          = 'Titre';
+$_['column_date']           = 'Date';
+$_['column_ip_adress']      = 'Adress IP';
+$_['column_url_modifie']    = 'Url modifiée';
+$_['column_user_id']        = 'id usager';
