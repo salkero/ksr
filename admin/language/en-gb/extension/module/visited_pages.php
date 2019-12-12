8d48b50a50cfbc5595ddd0f9bb979d86bdabb848
Salhi <e1895293@cmaisonneuve.qc.ca>
2019-12-12 15:38:28 -0500

    ajout et modification de fichiers pour le module

---
+++ admin/language/en-gb/extension/module/visited_pages.php
@@ -0,0 +1,16 @@
+<?php
+
+$_['']         = '';
+$_['heading_title']         = 'Visited Pages';
+$_['text_home']             = 'Visited Pages';
+
+
+// Column
+$_['column_url']            = 'Url';
+$_['column_title']          = 'Title';
+$_['column_date']           = 'Date';
+$_['column_ip_adress']      = 'IP adress';
+$_['column_url_modifie']    = 'Modified url';
+$_['column_user_id']        = 'User id';
+$_['column_nb_visit']       = 'Number of visits';
+
---
+++ admin/model/extension/module/visited.php
@@ -0,0 +1,70 @@
+<?php
+class ModelExtensionModuleVisited extends Model {
+    //fonction d'ajout
+    public function addVisite($url, $title, $date, $ip_adress, $user_id) {
+
+        $this->db->query("INSERT INTO ". DB_PREFIX . "visited (url, title, date, ip_adress, user_id) 
+                        VALUES (
+                         ".$this->db->escape($url).",
+                         ".$this->db->escape($title).",
+                         ".$this->db->escape($date).",
+                         ".$this->db->escape($ip_adress).",
+                         ".$this->db->escape($user_id).")");
+    }
+
+    //fonction de suppression                    
+    public function removeVisite($visited_id){
+
+        $this->db->query("DELETE FROM ".DB_PREFIX. "visited  WHERE visited_id = " .$visited_id);
+                            
+    }
+
+    //fonction de section de toute les visites. 
+    public function getAllVisitedPages(){
+        $resultat = $this->db->query("SELECT * FROM ". DB_PREFIX ."visited 
+                                    WHERE title != 'Your Store' 
+                                    AND title != 'Account Login' 
+                                    AND title != 'Account Logout' 
+                                    AND title != 'My Account'");
+        return $resultat->rows;
+     }
+     
+    // fonction de section des dernieres 15 visites. 
+    public function getLastFifteenMostVisited(){
+
+     
+        $resultat = $this->db->query("SELECT *, COUNT(*) as nb_visit 
+                                    FROM " .DB_PREFIX."visited 
+                                    WHERE title != 'Your Store' 
+                                    AND title != 'Account Login' 
+                                    AND title != 'Account Logout' 
+                                    AND title != 'My Account' 
+                                    GROUP BY url 
+                                    ORDER BY nb_visit 
+                                    DESC LIMIT 15");
+        return $resultat->rows;
+    }
+
+    public function install() {
+        
+        $this->db->query("CREATE TABLE IF NOT EXISTS " .DB_PREFIX. "visited (visited_id int NOT NULL AUTO_INCREMENT,
+        url varchar(150),
+        title varchar(35),
+        date DATETIME,
+        ip_address varchar(15),
+        user_id int,
+        url_modifie varchar(150),
+        PRIMARY KEY (visited_id))");
+       
+      
+    }
+    
+    public function uninstall() {
+
+        $this->db->query("DROP TABLE ".DB_PREFIX. "visited"); 
+      
+    }
+    
+
+
+}
