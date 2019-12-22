c88b068fb7cefc4a9d2b8cbf104a1c794aa39f1c
salkero <salkero@gmail.com>
2019-12-22 14:19:57 -0500

    ajout des entrées pour la langue francaise

--- admin/language/fr-fr/common/column_left.php
+++ admin/language/fr-fr/common/column_left.php
@@ -1,5 +1,3 @@
 <?php
 // Text
-$_['text_visited']              = 'Pages visitées';
-$_['text_visited_pages']        = 'Listes des pages visitées';
-$_['text_most_fifteen_visited_pages']         = 'les 15 pages les plus visitées';
+
