1673a76d160e4d58968f7f83ba17c9e43e944630
salkero <salkero@gmail.com>
2019-12-23 15:53:07 -0500

    ajout d'un fichier de langue englais pour la page des 15 plus visités

---
+++ admin/language/en-gb/extension/module/fifteen_most_visited_pages.php
@@ -0,0 +1,19 @@
+<?php
+
+$_['']         = '';
+$_['text_fifteen_most_visited_pages'] = 'Most fifteen visited pages';
+$_['heading_title']  = 'Visited pages Fifteen most';
+                   
+
+// Column
+$_['column_url']            = 'Url';
+$_['column_title']          = 'Title';
+$_['column_date']           = 'Date';
+$_['column_ip_adress']      = 'IP adress';
+$_['column_url_modifie']    = 'Modified url';
+$_['column_user_id']        = 'User id';
+$_['column_nb_visit']       = 'Number of visits';
+
+// Error
+$_['error_visited']         = 'please install the modules';
+
